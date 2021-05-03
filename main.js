f51d4d2e9614664eb0f8827f5a5fae5343e9332d
Pranav Chhabra <pranavchhabra@berkeley.edu>
2021-05-02 23:45:50 -0700

    Added working functionality for positive integers

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+function main(array) {
+  let retStr = "";
 
+  var dict = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 0: "Zero"};
+  var i;
+
+  for (i = 0; i < array.length; i++) {
+    var currentString = "";
+    var currentNum = array[i];
+
+    while (currentNum > 0) {
+      var lastDigit = currentNum % 10;
+      currentString = dict[lastDigit] + currentString;
+
+      currentNum = Math.floor(currentNum/10);
+    }
+    if (i != array.length - 1) {
+      currentString += ',';
+    }
+    retStr = retStr + currentString;
+  }
+  return retStr;
+}
+
+var num_array = process.argv.slice(2);
+console.log(main(num_array));
